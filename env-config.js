9234265fc122c6083fe5b7e1de9358cf9546452b
Remi <romios047@gmail.com>
2019-10-04 14:52:13 +0300

    changed namespace

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
-
+// https://roman-batiuk.herokuapp.com
 module.exports = {
-    'process.env.BASE_URL': prod ? 'https://roman-batiuk.herokuapp.com' : 'http://localhost:3000',
-    'process.env.NAMESPACE': 'https://roman-batiuk.herokuapp.com',
+    'process.env.BASE_URL': prod ? 'https://portfolio.romios047.now.sh' : 'http://localhost:3000',
+    'process.env.NAMESPACE': 'https://portfolio.romios047.now.sh',
     'process.env.CLIENT_ID': 'NF446kLamlii1MZ37Gt3664aY0p5nOaz',
 };
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGO_URI : process.env.MONGO_URI,
-    NAMESPACE: 'https://roman-batiuk.herokuapp.com'
+    MONGO_URI : 'mongodb+srv://webremi42:LteOc3tHJFWHlhBe@cluster0-6zlep.mongodb.net/portfolio-app?retryWrites=true&w=majority',
+    NAMESPACE: 'https://portfolio.romios047.now.sh'
 };
